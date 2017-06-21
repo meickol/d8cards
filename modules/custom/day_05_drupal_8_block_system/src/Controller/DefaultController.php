fbbfb09dee924fa3bcf7a91e7b809ab8a321479a
Maicol Lopez <maicol.lopes@jobsity.com>
2017-06-20 22:41:31 -0500

    Day 3

--- modules/custom/day_05_drupal_8_block_system/src/Controller/DefaultController.php
+++ modules/custom/day_05_drupal_8_block_system/src/Controller/DefaultController.php
@@ -18,9 +18,26 @@ class DefaultController extends ControllerBase {
    *   Return Hello string.
    */
   public function test() {
+
+
+    // Get blocks list
+    $blocks = \Drupal::entityTypeManager()
+      ->getStorage('block_content')
+      ->loadByProperties(array('type' => 'stock_exchange_rate_card'));
+
+
+
+      // Get field_symbol value.
+      $block_symbol = $blocks[1]->field_symbol->value;
+      //dump($blocks[1]->field_symbol);
+
+    $admin_information = \Drupal::config('day_3_building_configuration_forms.my_config_module');
+
+    dump($admin_information->get('gender'));
+
     return [
       '#type' => 'markup',
-      '#markup' => $this->t('Implement method: test'),
+      '#markup' => $this->t('Admin Name is @name; Admin shoe size is @shoe_size; Admin gender is @gender', ['@name' => $admin_information->get('name'),'@shoe_size' => $admin_information->get('shoe_size'),'@gender' => $admin_information->get('gender')]),
     ];
   }
 
---
+++ modules/custom/day_3_building_configuration_forms/src/Forms/ConfigForm.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\day_3_building_configuration_forms\Forms;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class ConfigForm
+ *
+ * @package Drupal\day_3_building_configuration_forms\Forms
+ */
+class ConfigForm extends ConfigFormBase {
+  protected function getEditableConfigNames() {
+    return ['day_3_building_configuration_forms.my_config_module'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'day_3_building_configuration_forms_my_config_module';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('day_3_building_configuration_forms.my_config_module');
+
+    $form['name'] = [
+      '#type' => 'textfield',
+      '#title' => t('Name'),
+      '#default_value' => $config->get('name'),
+      '#required' => TRUE,
+    ];
+
+    $options = [9, 10, 11, 12];
+    $form['shoe_size'] = [
+      '#type' => 'select',
+      '#title' => t('Shoe Size'),
+      '#default_value' => $config->get('shoe_size'),
+      '#options' => $options,
+      '#required' => TRUE,
+    ];
+
+    $gender_options = [
+      'F' => $this->t('F'),
+      'M' => $this->t('M'),
+      'Other' => $this->t('Other')
+    ];
+
+    $form['gender'] = [
+      '#type' => 'radios',
+      '#title' => t('Gender'),
+      '#options' => $gender_options,
+      '#default_value' => $config->get('gender'),
+      '#required' => TRUE,
+    ];
+
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $config = $this->config('day_3_building_configuration_forms.my_config_module')
+      ->set('name', $form_state->getValue('name'))
+      ->set('shoe_size', $form_state->getValue('shoe_size'))
+      ->set('gender', $form_state->getValue('gender'));
+
+    $config->save();
+
+    drupal_set_message(t('Your configurations have been saved.'));
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
