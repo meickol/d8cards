1ce1481d56c53c4640b4b77c36b9746d6b43a08e
Maicol Lopez <maicol.lopez@jobsity.com>
2017-10-06 03:14:42 -0500

    Day 21 - dependency injection to subscribe service

--- modules/custom/day_21_replacing_hook_init/src/ResponseSubscriber.php
+++ modules/custom/day_21_replacing_hook_init/src/ResponseSubscriber.php
@@ -2,11 +2,22 @@
 
 namespace Drupal\day_21_replacing_hook_init;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 
-class ResponseSubscriber implements EventSubscriberInterface{
+class ResponseSubscriber implements EventSubscriberInterface {
+
+  private $currentUser;
+
+  public function __construct($current_user) {
+    $this->currentUser = $current_user;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('current_user'));
+  }
 
   public static function getSubscribedEvents() {
     return [
@@ -17,11 +28,14 @@ class ResponseSubscriber implements EventSubscriberInterface{
 
   public function onResponse(FilterResponseEvent $event) {
 
-    if (\Drupal::currentUser()->isAnonymous()) {
+    if ($this->currentUser->isAnonymous()) {
       $response = $event->getResponse();
       $response->headers->set('Access-Control-Allow-Origin', '*');
     }
 
   }
+
+
+
 
 }
